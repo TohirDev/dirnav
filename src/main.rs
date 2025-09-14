2b7848399049a0440cfc4285977cbc5ad41fcc65
TohirDev <isakulovtohir902@gmail.com>
2025-09-14 09:29:09 +0500

    added basic funcs to navigate up and down

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-use std::{
-    env,
-    ffi::{OsStr, OsString},
-    fs,
-};
+use std::{env, ffi::OsString, fs};
 
 use color_eyre::eyre::{Ok, Result};
 use ratatui::{
     DefaultTerminal, Frame,
     crossterm::event::{self, Event},
     layout::{Constraint, Layout},
-    style::{Color, Stylize},
-    widgets::{Block, BorderType, List, ListItem, Paragraph, Widget},
+    style::{Color, Style, Stylize},
+    widgets::{Block, BorderType, List, ListItem, ListState, Paragraph, Widget},
 };
+
+#[derive(Debug, Default)]
+struct AppState {
+    dirs: Vec<OsString>,
+    lists: ListState,
+}
 
 fn main() -> Result<()> {
     color_eyre::install()?;
@@ -23,26 +25,32 @@ fn main() -> Result<()> {
 
 fn run(mut terminal: DefaultTerminal) -> Result<()> {
     let path = env::current_dir()?;
-    let mut dirs = vec![];
+    let mut app_state = AppState::default();
     for entry in fs::read_dir(&path)? {
         let entry = entry?;
         let path = entry.path();
         let file_name = path.file_name().unwrap();
         if path.is_dir() {
-            dirs.push(file_name.to_owned());
+            app_state.dirs.push(file_name.to_owned());
         }
     }
 
     loop {
-        terminal.draw(|f| render(f, &dirs))?;
+        terminal.draw(|f| render(f, &mut app_state))?;
         if let Event::Key(k) = event::read()? {
             match k.code {
                 event::KeyCode::Esc => {
                     break;
                 }
-                // event::KeyCode::Char(char) => match char {
-                //     ""
-                // },
+                event::KeyCode::Char(char) => match char {
+                    'j' => {
+                        app_state.lists.select_next();
+                    }
+                    'k' => {
+                        app_state.lists.select_previous();
+                    }
+                    _ => {}
+                },
                 _ => {}
             }
         }
@@ -50,7 +58,7 @@ fn run(mut terminal: DefaultTerminal) -> Result<()> {
     Ok(())
 }
 
-fn render(frame: &mut Frame, list: &Vec<OsString>) -> () {
+fn render(frame: &mut Frame, state: &mut AppState) -> () {
     let [border_area] = Layout::vertical([Constraint::Fill(1)])
         .margin(1)
         .areas(frame.area());
@@ -62,22 +70,12 @@ fn render(frame: &mut Frame, list: &Vec<OsString>) -> () {
         .fg(Color::Yellow)
         .render(border_area, frame.buffer_mut());
     let lists = List::new(
-        list.iter()
+        state
+            .dirs
+            .iter()
             .map(|x| ListItem::from(x.to_string_lossy().to_string())),
     )
-    .render(inner_area, frame.buffer_mut());
-    // frame.render_stateful_widget(&lists, inner_area, & list);
+    .highlight_symbol(">")
+    .highlight_style(Style::default().fg(Color::Green));
+    frame.render_stateful_widget(lists, inner_area, &mut state.lists);
 }
-
-// let path = env::current_dir()?;
-// for entry in fs::read_dir(&path)? {
-//     let entry = entry?;
-//     let path = entry.path();
-
-//     if path.is_dir() {
-//         println!("Dir found: {:?}", path.file_name().unwrap())
-//     } else if path.is_file() {
-//         println!("File found: {:?}", path.file_name().unwrap())
-//     }
-// }
-// println!("{:?}", fs::read_dir(&path));
